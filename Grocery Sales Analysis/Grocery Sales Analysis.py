dfe7ea972bd1e9e6a0b0ad6a72aa9442d5b66ca9
Lydiah Moraa <lydiahmorah@gmail.com>
2024-06-16 01:27:57 +0300

    Intial Commit

--- Grocery Sales Analysis/Grocery Sales Analysis.py
+++ Grocery Sales Analysis/Grocery Sales Analysis.py
@@ -19,3 +19,14 @@ plt.xlabel('Product Type')
 plt.ylabel('Total Sales')
 plt.title('Total Sales by Product Type')
 plt.show()
+
+
+#Box Plot: Price Distribution by Brand
+plt.figure(figsize=(16, 10))
+palette = sns.color_palette("Spectral", 10)  
+sns.boxplot(x='brand', y='price', data=df_products, palette='bright')
+plt.xlabel('Brand')
+plt.ylabel('Price')
+plt.title('Price Distribution by Brand')
+plt.xticks(rotation=45)
+plt.show()
